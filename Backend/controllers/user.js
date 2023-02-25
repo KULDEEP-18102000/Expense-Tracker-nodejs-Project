fbfaa887f289ab08d4a13ac0a753a9efd72781ca
KULDEEP-18102000 <jadonkuldeepsingh2@gmail.com>
2023-02-25 20:54:28 +0530

    login backend implemented

--- Backend/controllers/user.js
+++ Backend/controllers/user.js
@@ -5,7 +5,7 @@ exports.signUp=async(req,res,next)=>{
     const name=req.body.name
     const email=req.body.email
     const password=req.body.password
-    const user =await User.findOne({email:email})
+    const user =await User.findOne({where:{email:email}})
     if(user){
         console.log("User already exists")
         res.json({"message":"user already exists"})
@@ -19,4 +19,17 @@ exports.signUp=async(req,res,next)=>{
     })
     }
     
+}
+
+exports.login=async(req,res,next)=>{
+    const email=req.body.email
+    const password=req.body.password
+    const user=await User.findOne({where:{email:email}})
+    if(!user){
+        res.status(404).json({"message":"user not found"})
+    }else if(user.password!==password){
+        res.status(401).json({"message":"user is not authorized"})
+    }else{
+        res.status(200).json({"message":"user login successfull"})
+    }
 }
--- Backend/routes/user.js
+++ Backend/routes/user.js
@@ -2,6 +2,8 @@ const express=require('express')
 const router=express.Router();
 const userController=require('../controllers/user');
 
-router.post('/',userController.signUp)
+router.post('/signup',userController.signUp)
+
+router.post('/login',userController.login)
 
 module.exports=router;
