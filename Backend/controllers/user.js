d60f78bd83541f0903d4768d98ccfb58a1b27979
KULDEEP-18102000 <jadonkuldeepsingh2@gmail.com>
2023-02-26 09:02:46 +0530

    login and signup with bcrypt password encryption

--- Backend/controllers/user.js
+++ Backend/controllers/user.js
@@ -1,23 +1,27 @@
 const User=require('../models/user')
+const bcrypt=require('bcryptjs')
 
 exports.signUp=async(req,res,next)=>{
     // console.log(req.body)
-    const name=req.body.name
+    try {
+        const name=req.body.name
     const email=req.body.email
     const password=req.body.password
     const user =await User.findOne({where:{email:email}})
     if(user){
         console.log("User already exists")
-        res.json({"message":"user already exists"})
+        res.json({message:"user already exists"})
     }else{
-        User.create({name:name,email:email,password:password})
-    .then(user=>{
-        res.json(user)
-    })
-    .catch(err=>{
-        console.log(err)
-    })
+        bcrypt.hash(password,10,async(err,hash)=>{
+            console.log(err)
+            await User.create({name:name,email:email,password:hash})
+            res.status(201).json({message:"successfully created new user"})
+        })
     }
+    } catch (error) {
+        res.status(500).json(err)
+    }
+    
     
 }
 
@@ -26,10 +30,19 @@ exports.login=async(req,res,next)=>{
     const password=req.body.password
     const user=await User.findOne({where:{email:email}})
     if(!user){
-        res.status(404).json({"message":"user not found"})
-    }else if(user.password!==password){
-        res.status(401).json({"message":"user is not authorized"})
-    }else{
-        res.status(200).json({"message":"user login successfull"})
+        res.status(404).json({message:"user not found"})
     }
+    else{
+        bcrypt.compare(password,user.password,(err,result)=>{
+            if(err){
+                res.status(500).json({success:false, message:"Something went wrong"})
+            }
+            if(result===true){
+                res.status(200).json({success:true,message:"user login successfull"})
+            }else{
+                res.status(400).json({success:false,message:"incorrect password"})
+            }
+        })
+    }
+    
 }
